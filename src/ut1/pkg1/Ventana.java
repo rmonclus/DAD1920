26c31272dd2c8c1a851d7421bd80e55084ec78d6
rmonclus <rubisco@DESKTOP-VH7TOB5>
2019-09-14 21:48:53 +0100

    1st version

---
+++ src/ut1/pkg1/Ventana.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ut1.pkg1;
+
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author rubisco
+ */
+public class Ventana extends JFrame {
+    
+    public Ventana() {
+        setSize(new Dimension(640,480));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+}
